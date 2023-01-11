3cf162a2df57b347fc142c16068f891b42b0fbf4
Anand Bajpai <anand.bajpai@1mg.com>
2023-01-11 12:07:21 +0530

    add framework

---
+++ Validator/Validator/MailValidator.swift
@@ -0,0 +1,16 @@
+//
+//  MailValidator.swift
+//  Validator
+//
+//  Created by anand on 11/01/23.
+//
+
+import Foundation
+
+public struct MailValidator {
+  
+  public static func validMail() {
+    print("This is... I don't know")
+  }
+  
+}
